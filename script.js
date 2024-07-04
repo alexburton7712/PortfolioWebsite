f1e497a2e85b433771a39e06eaad3b0763328b4e
alexburton7712 <alexburton7712@icloud.com>
2024-07-04 01:08:55 -0700

    started developing typing animation

---
+++ script.js
@@ -0,0 +1,14 @@
+function typeWriter() {
+    var i = 0
+    var paragraph = document.getElementById("about-me-text")
+    var text = "hello.\nmy name is alex burton."
+    var speed = 1;
+
+    if (i < text.length) {
+        paragraph.innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+      }
+}
+
+document.addEventListener('DOMContentLoaded', typeWriter())
